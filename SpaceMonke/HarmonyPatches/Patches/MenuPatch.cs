0b8460c5894c8e0f52057d58e358002aba73251b
Kieran Vey <kieranvey@gmail.com>
2022-03-15 19:59:47 -0300

    integrated platformmonke

--- SpaceMonke/HarmonyPatches/Patches/MenuPatch.cs
+++ SpaceMonke/HarmonyPatches/Patches/MenuPatch.cs
@@ -8,6 +8,8 @@ using System.IO;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
 using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
 
 namespace ModMenuPatch.HarmonyPatches
 {
@@ -17,8 +19,14 @@ namespace ModMenuPatch.HarmonyPatches
     {
         public static bool ResetSpeed = false;
 
-        static string[] buttons = new string[] { "Toggle Super Monke", "Toggle Tag Gun", "Toggle Speed Boost", "Tag All", "Turn Off Tag Freeze", "Toggle Beacon", "+10 speed", "-10 speed" };
-        static bool?[] buttonsActive = new bool?[] { false, false, false, false, false, false, false, false };
+        static string[] buttons = new string[] { "Toggle Super Monke", "Toggle Tag Gun", "Toggle Speed Boost", "Tag All", "Turn Off Tag Freeze", "Toggle Beacon", "Platform Monke" };
+        static bool?[] buttonsActive = new bool?[] { false, false, false, false, false, false, false };
+
+        //static string[] buttons = new string[] { "Toggle Super Monke", "Toggle Tag Gun", "Toggle Speed Boost", "Tag All", "Turn Off Tag Freeze", "Toggle Beacon", "+10 speed", "-10 speed" };
+        //static bool?[] buttonsActive = new bool?[] { false, false, false, false, false, false, false, false };
+
+        //static string[] buttons = new string[] { "Toggle Super Monke", "Toggle Tag Gun", "Toggle Speed Boost", "Tag All", "Turn Off Tag Freeze", "Toggle Beacon" };
+        //static bool?[] buttonsActive = new bool?[] { false, false, false, false, false, false };
         static bool gripDown;
         static GameObject menu = null;
         static GameObject canvasObj = null;
@@ -54,7 +62,7 @@ namespace ModMenuPatch.HarmonyPatches
 
                 List<UnityEngine.XR.InputDevice> list = new List<UnityEngine.XR.InputDevice>();
                 InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Left | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
-                list[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out gripDown);
+                list[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out gripDown);
 
                 if (gripDown && menu == null)
                 {
@@ -241,25 +249,31 @@ namespace ModMenuPatch.HarmonyPatches
 
                 if (buttonsActive[6] == true)
                 {
-                    if (speedPlusCooldown == 0)
-                    {
-                        speedPlusCooldown = Time.frameCount + 30;
-                        ModMenuPatch.speedMultiplier.Value += 10;
-                        buttons[6] = "+10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
-                        buttons[7] = "-10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
-                    }
+                    ProcessPlatformMonke();
                 }
-                                
-                if (buttonsActive[7] == true)
-                {
-                    if (speedMinusCooldown == 0)
-                    {
-                        speedMinusCooldown = Time.frameCount + 30;
-                        ModMenuPatch.speedMultiplier.Value -= 10;
-                        buttons[7] = "-10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
-                        buttons[6] = "+10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
-                    }
-                }
+
+
+                //if (buttonsActive[6] == true)
+                //{
+                //    if (speedPlusCooldown == 0)
+                //    {
+                //        speedPlusCooldown = Time.frameCount + 30;
+                //        ModMenuPatch.speedMultiplier.Value += 10;
+                //        buttons[6] = "+10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
+                //        buttons[7] = "-10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
+                //    }
+                //}
+
+                //if (buttonsActive[7] == true)
+                //{
+                //    if (speedMinusCooldown == 0)
+                //    {
+                //        speedMinusCooldown = Time.frameCount + 30;
+                //        ModMenuPatch.speedMultiplier.Value -= 10;
+                //        buttons[7] = "-10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
+                //        buttons[6] = "+10 speed (" + ModMenuPatch.speedMultiplier.Value.ToString() + ")";
+                //    }
+                //}
 
                 if (btnCooldown > 0)
                 {
@@ -273,29 +287,29 @@ namespace ModMenuPatch.HarmonyPatches
                     }
                 }
 
-                if (speedPlusCooldown > 0)
-                {
-                    if (Time.frameCount > speedPlusCooldown)
-                    {
-                        speedPlusCooldown = 0;
-                        buttonsActive[6] = false;
-                        GameObject.Destroy(menu);
-                        menu = null;
-                        Draw();
-                    }
-                }
+                //if (speedPlusCooldown > 0)
+                //{
+                //    if (Time.frameCount > speedPlusCooldown)
+                //    {
+                //        speedPlusCooldown = 0;
+                //        buttonsActive[6] = false;
+                //        GameObject.Destroy(menu);
+                //        menu = null;
+                //        Draw();
+                //    }
+                //}
 
-                if (speedMinusCooldown > 0)
-                {
-                    if (Time.frameCount > speedMinusCooldown)
-                    {
-                        speedMinusCooldown = 0;
-                        buttonsActive[7] = false;
-                        GameObject.Destroy(menu);
-                        menu = null;
-                        Draw();
-                    }
-                }
+                //if (speedMinusCooldown > 0)
+                //{
+                //    if (Time.frameCount > speedMinusCooldown)
+                //    {
+                //        speedMinusCooldown = 0;
+                //        buttonsActive[7] = false;
+                //        GameObject.Destroy(menu);
+                //        menu = null;
+                //        Draw();
+                //    }
+                //}
 
 
                 //if (Time.frameCount % 4000 == 0)
@@ -309,6 +323,178 @@ namespace ModMenuPatch.HarmonyPatches
             }
 
         }
+
+
+
+        #region platform monke stuff
+        static Vector3 scale = new Vector3(0.0125f, 0.28f, 0.3825f);
+        static bool gripDown_left;
+        static bool gripDown_right;
+        static bool once_left;
+        static bool once_right;
+        static bool once_left_false;
+        static bool once_right_false;
+        static bool once_networking;
+        static GameObject[] jump_left_network = new GameObject[9999];
+        static GameObject[] jump_right_network = new GameObject[9999];
+        static GameObject jump_left_local = null;
+        static GameObject jump_right_local = null;
+
+        static void ProcessPlatformMonke()
+        {
+            if (!once_networking)
+            {
+                PhotonNetwork.NetworkingClient.EventReceived += PlatformNetwork;
+                once_networking = true;
+            }
+            List<UnityEngine.XR.InputDevice> list = new List<UnityEngine.XR.InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Left | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
+            list[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out gripDown_left);
+            InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
+            list[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out gripDown_right);
+
+            if (gripDown_right) //Right hand - I'm sorry for putting if statements within if statements... OR ELSE IT WOULN'T WORK :<
+            {
+                if (!once_right)
+                {
+                    if (jump_right_local == null)
+                    {
+                        jump_right_local = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        jump_right_local.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                        jump_right_local.transform.localScale = scale;
+                        jump_right_local.transform.position = new Vector3(0, (float)-0.0075, 0) + GorillaLocomotion.Player.Instance.rightHandTransform.position; //The reason for moving it down a little, is because on the right hand the cube would spawn ontop of the hand for some reason.
+                        jump_right_local.transform.rotation = GorillaLocomotion.Player.Instance.rightHandTransform.rotation;
+
+                        object[] right_form_1 = new object[] { new Vector3(0, (float)-0.0075, 0) + GorillaLocomotion.Player.Instance.rightHandTransform.position, GorillaLocomotion.Player.Instance.rightHandTransform.rotation };
+
+                        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+
+                        PhotonNetwork.RaiseEvent((byte)PhotonEventCodes.right_jump_photoncode, right_form_1, raiseEventOptions, SendOptions.SendReliable);
+
+                        once_right = true;
+                        once_right_false = false;
+                    }
+                }
+            }
+            else
+            {
+                if (!once_right_false)
+                {
+                    if (jump_right_local != null)
+                    {
+                        GameObject.Destroy(jump_right_local);
+                        jump_right_local = null;
+
+                        once_right = false;
+                        once_right_false = true;
+
+                        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+
+                        PhotonNetwork.RaiseEvent((byte)PhotonEventCodes.right_jump_deletion, null, raiseEventOptions, SendOptions.SendReliable);
+                    }
+                }
+            }
+
+            if (gripDown_left) //Left hand - I'm sorry for putting if statements within if statements... OR ELSE IT WOULN'T WORK :<
+            {
+                if (!once_left)
+                {
+                    if (jump_left_local == null)
+                    {
+                        jump_left_local = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        jump_left_local.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                        jump_left_local.transform.localScale = scale;
+                        jump_left_local.transform.position = GorillaLocomotion.Player.Instance.leftHandTransform.position;
+                        jump_left_local.transform.rotation = GorillaLocomotion.Player.Instance.leftHandTransform.rotation;
+
+                        object[] left_form_1 = new object[] { GorillaLocomotion.Player.Instance.leftHandTransform.position, GorillaLocomotion.Player.Instance.leftHandTransform.rotation };
+
+                        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+
+                        PhotonNetwork.RaiseEvent((byte)PhotonEventCodes.left_jump_photoncode, left_form_1, raiseEventOptions, SendOptions.SendReliable);
+
+                        once_left = true;
+                        once_left_false = false;
+                    }
+                }
+            }
+            else
+            {
+                if (!once_left_false)
+                {
+                    if (jump_left_local != null)
+                    {
+                        GameObject.Destroy(jump_left_local);
+                        jump_left_local = null;
+
+                        once_left = false;
+                        once_left_false = true;
+
+                        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+
+                        PhotonNetwork.RaiseEvent((byte)PhotonEventCodes.left_jump_deletion, null, raiseEventOptions, SendOptions.SendReliable);
+                    }
+                }
+            }
+
+            if (!PhotonNetwork.InRoom) //Worst way of doing it, but I'm not even bothered lol.
+            {
+                for (int i = 0; i < jump_right_network.Length; i++)
+                {
+                    GameObject.Destroy(jump_right_network[i]);
+                }
+
+                for (int i = 0; i < jump_left_network.Length; i++)
+                {
+                    GameObject.Destroy(jump_left_network[i]);
+                }
+            }
+
+        }
+
+        public enum PhotonEventCodes //Stores all the Photon codes, if you're making a mod do NOT use these photon values.
+        {
+            left_jump_photoncode = 69,
+            right_jump_photoncode = 70,
+            left_jump_deletion = 71,
+            right_jump_deletion = 72
+        }
+
+        static private void PlatformNetwork(EventData eventData)
+        {
+            byte eventCode = eventData.Code;
+            if (eventCode == (byte)PhotonEventCodes.left_jump_photoncode)
+            {
+                object[] data_left = (object[])eventData.CustomData; //Array
+                jump_left_network[eventData.Sender] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                jump_left_network[eventData.Sender].GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                jump_left_network[eventData.Sender].transform.localScale = scale;
+                jump_left_network[eventData.Sender].transform.position = (Vector3)data_left[0]; //Sets position.
+                jump_left_network[eventData.Sender].transform.rotation = (Quaternion)data_left[1]; //Sets rotation.
+            }
+            else if (eventCode == (byte)PhotonEventCodes.right_jump_photoncode)
+            {
+                object[] data_right = (object[])eventData.CustomData; //Array
+                jump_right_network[eventData.Sender] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                jump_right_network[eventData.Sender].GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                jump_right_network[eventData.Sender].transform.localScale = scale;
+                jump_right_network[eventData.Sender].transform.position = (Vector3)data_right[0]; //Sets position.
+                jump_right_network[eventData.Sender].transform.rotation = (Quaternion)data_right[1]; //Sets rotation.
+            }
+            else if (eventCode == (byte)PhotonEventCodes.left_jump_deletion)
+            {
+                GameObject.Destroy(jump_left_network[eventData.Sender]);
+                jump_left_network[eventData.Sender] = null;
+            }
+            else if (eventCode == (byte)PhotonEventCodes.right_jump_deletion)
+            {
+                GameObject.Destroy(jump_right_network[eventData.Sender]);
+                jump_right_network[eventData.Sender] = null;
+            }
+        }
+
+        #endregion
+
 
         static void AddButton(float offset, string text)
         {
@@ -366,15 +552,19 @@ namespace ModMenuPatch.HarmonyPatches
             GameObject.Destroy(menu.GetComponent<Rigidbody>());
             GameObject.Destroy(menu.GetComponent<BoxCollider>());
             GameObject.Destroy(menu.GetComponent<Renderer>());
+
             //menu.transform.localScale = new Vector3(0.1f, 0.3f, 0.4f);
-            menu.transform.localScale = new Vector3(0.1f, 0.3f, 0.7f);
+            menu.transform.localScale = new Vector3(0.1f, 0.3f, 0.4f);
 
             GameObject background = GameObject.CreatePrimitive(PrimitiveType.Cube);
             GameObject.Destroy(background.GetComponent<Rigidbody>());
             GameObject.Destroy(background.GetComponent<BoxCollider>());
             background.transform.parent = menu.transform;
             background.transform.rotation = Quaternion.identity;
-            background.transform.localScale = new Vector3(0.1f, 1f, 1f);
+
+            //background.transform.localScale = new Vector3(0.1f, 1f, 1f);
+            background.transform.localScale = new Vector3(0.1f, 1f, 1.25f);
+
             background.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
             background.transform.position = new Vector3(0.05f, 0f, 0f);
 
@@ -390,7 +580,7 @@ namespace ModMenuPatch.HarmonyPatches
             titleObj.transform.parent = canvasObj.transform;
             Text title = titleObj.AddComponent<Text>();
             title.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-            title.text = "Quandle Dingle mods";
+            title.text = "Monke mod menu";
             title.fontSize = 1;
             title.alignment = TextAnchor.MiddleCenter;
             title.resizeTextForBestFit = true;
